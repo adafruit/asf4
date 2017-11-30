90c4f88227d5695f6419b34a206315e79b236579
Dan Halbert <halbert@halwitz.org>
2017-11-29 21:59:31 -0500

    add a SERCOM that uses the async USART driver

--- samd21/examples/driver_examples.c
+++ samd21/examples/driver_examples.c
@@ -62,14 +62,33 @@ void SPI_0_example(void)
 	io_write(io, example_SPI_0, 12);
 }
 
-void I2C_0_example(void)
-{
-	struct io_descriptor *I2C_0_io;
+/**
+ * Example of using USART_1 to write "Hello World" using the IO abstraction.
+ *
+ * Since the driver is asynchronous we need to use statically allocated memory for string
+ * because driver initiates transfer and then returns before the transmission is completed.
+ *
+ * Once transfer has been completed the tx_cb function will be called.
+ */
 
-	i2c_m_sync_get_io_descriptor(&I2C_0, &I2C_0_io);
-	i2c_m_sync_enable(&I2C_0);
-	i2c_m_sync_set_slaveaddr(&I2C_0, 0x12, I2C_M_SEVEN);
-	io_write(I2C_0_io, (uint8_t *)"Hello World!", 12);
+static uint8_t example_USART_1[12] = "Hello World!";
+
+static void tx_cb_USART_1(const struct usart_async_descriptor *const io_descr)
+{
+	/* Transfer completed */
+}
+
+void USART_1_example(void)
+{
+	struct io_descriptor *io;
+
+	usart_async_register_callback(&USART_1, USART_ASYNC_TXC_CB, tx_cb_USART_1);
+	/*usart_async_register_callback(&USART_1, USART_ASYNC_RXC_CB, rx_cb);
+	usart_async_register_callback(&USART_1, USART_ASYNC_ERROR_CB, err_cb);*/
+	usart_async_get_io_descriptor(&USART_1, &io);
+	usart_async_enable(&USART_1);
+
+	io_write(io, example_USART_1, 12);
 }
 
 /**
@@ -82,6 +101,16 @@ void USART_0_example(void)
 	usart_sync_enable(&USART_0);
 
 	io_write(io, (uint8_t *)"Hello World!", 12);
+}
+
+void I2C_0_example(void)
+{
+	struct io_descriptor *I2C_0_io;
+
+	i2c_m_sync_get_io_descriptor(&I2C_0, &I2C_0_io);
+	i2c_m_sync_enable(&I2C_0);
+	i2c_m_sync_set_slaveaddr(&I2C_0, 0x12, I2C_M_SEVEN);
+	io_write(I2C_0_io, (uint8_t *)"Hello World!", 12);
 }
 
 /**
